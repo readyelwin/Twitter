799c28ecf1627a1dbdd2f5569ff546d45ece3374
Elwin <readyelwin@gmail.com>
2020-03-23 20:28:56 -0400

    twitter

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -17,6 +17,13 @@ class TweetViewController: UIViewController {
     }
     
 
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func tweet(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
