e53e19599612f8e95039899edd361867779c051a
elwin <readyelwin@gmail.com>
2020-03-25 01:53:12 -0400

    twitter

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -89,5 +89,14 @@ class TwitterAPICaller: BDBOAuth1SessionManager {
                 failure(error)
             })
         }
-            }
+            
+    
+    func retweet(tweetId:Int, success: @escaping () -> (), failure: @escaping (Error) -> ()){
+        let url = "https://api.twitter.com/1.1/retweet/\(tweetId).json"
+        TwitterAPICaller.client?.post(url, parameters:["id":tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
+            success()
+        }, failure: { (task: URLSessionDataTask?, error: Error) in
+            failure(error)
+        })
+    }    }
 }
