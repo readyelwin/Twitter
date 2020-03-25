b0bcae4a7d508c75108c8a74f6bfef4bff84abc4
elwin <readyelwin@gmail.com>
2020-03-25 01:21:46 -0400

    twitter

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -18,13 +18,32 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var retweetButton: UIButton!
     
     @IBOutlet weak var favButton: UIButton!
+    var favorited:Bool = false
+    var tweetId:Int = -1
     
     @IBAction func favoriteTweet(_ sender: Any) {
-    }
-    
-    @IBAction func retweet(_ sender: Any) {
         
-        var favorited:Bool = false
+        let tobeFavorited = !favorited
+        if(tobeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: , success:{
+                self.setFavorite(true)
+            } , failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        } else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("Unfavorite did not succeed: \(error)")
+        
+        })
+        }
+        
+        
+        }
+    
+        func retweet(_ sender: Any) {
+        
         
         func setFavorite(_ isFavorited:Bool) {
             favorited = isFavorited
